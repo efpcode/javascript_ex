1d427f895b75eb8ab1ac4cec9331a9167f8dca33
minor-e <estebanatmail@gmail.com>
2019-06-10 14:35:11 +0200

    Removed a for loop and simplified function

--- javascript/functions/javascript/howmanyletters.js
+++ javascript/functions/javascript/howmanyletters.js
@@ -1,30 +1,23 @@
 
 function isAlpha(ch){
-	var letters ="";
-	var i;
-	for(i = 0; i < ch.length; i++){
-		if(/_/.test(ch[i])){
-			continue
-		}
-		else if ( /^\W/.test(ch[i])){
-			continue;
-		}
-		else if (/^[A-za-z]+$/.test(ch[i])){
-			letters += ch[i];
-		}
-		else{
-			var letters = "Good job You Tricked Me!";
+	var ch = ch.split(" ").join("");
+	var regex = /[a-z]/gi;
+	var letters = ch.match(regex);
 
-			return [letters, letters.length ] 
+	if (!(letters)){
+		return [ch, "Empty string can't be processed!"];
+	}
+
+
+	else if (!(ch.length===letters.length)){
+		return [ch, "Not a vaild input only letter A-Z or a-z"];
 		}
-	};
-	return [letters, letters.length];
+	
+	else{
+		return [letters, letters.length ] 
+		}
 };
 var input_text = prompt("I can only count letters!", "Try to trick me");
 document.body.innerHTML = "<div> There are this many letters in " + input_text + " see line below!</div>";
 var letters  = isAlpha(input_text);
 document.body.innerHTML += "letters: "+ letters[0] + " length: " + letters[1];
-
-
-
-
